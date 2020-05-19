8630f31ceadf0e190bd532e77fb63b3547c2cb21
sachin <awsiot-freertos-apn@librewireless.com>
2020-05-19 16:20:25 +0530

    remove shell command check in app load batch file

--- vendors/libre/mavid/tools/loadApp.bat
+++ vendors/libre/mavid/tools/loadApp.bat
@@ -1,10 +1,4 @@
 echo off
-If /I "%COMSPEC%" == %CMDCMDLINE% Goto runBat
-echo Run from Command Prompt....
-PAUSE
-goto :eof
-
-:runBat
 if [%1]==[] goto noArg
 echo "Flashing " %1\projects\libre\mavid\iar\aws_tests\Debug\Exe\aws_tests.bin_JTAG.bin
 ST-LINK_CLI.exe -SE 6 11  -P %1\projects\libre\mavid\iar\aws_tests\Debug\Exe\aws_tests.bin_JTAG.bin 0x08080000 
